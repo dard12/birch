73145afb529f9404e2d7332ba9deb570ddb49ea6
Ricky Lung <dard12@gmail.com>
2019-11-03 21:32:48 -0500

    start creating a journal

--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import styles from './Home.module.scss';
-import { userSelector } from '../../redux/selectors';
 import RichText from '../../components/RichText/RichText';
 
-interface HomeProps {
-  user?: string;
-}
+interface HomeProps {}
 
 function Home(props: HomeProps) {
-  const { user } = props;
-
   return (
     <div className={styles.homePage}>
-      <RichText />
+      <div className={styles.journal}>
+        <RichText placeholder="What are you thinking?" />
+      </div>
     </div>
   );
 }
 
-export default connect(userSelector)(Home);
+export default Home;
