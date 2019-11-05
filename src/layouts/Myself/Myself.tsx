97bef43de9e11dc070fcb2774bf1e1274e8bd4fe
Ricky Lung <dard12@gmail.com>
2019-11-04 21:01:53 -0500

    slight heading style

--- src/layouts/Myself/Myself.tsx
+++ src/layouts/Myself/Myself.tsx
@@ -8,37 +8,37 @@ function Myself(props: MyselfProps) {
   return (
     <div className={styles.myselfPage}>
       <div>
-        <div className="heading-1">Values</div>
         <div className={styles.journal}>
+          <div className="heading-1">Values</div>
           <RichText placeholder="What are your values?" />
         </div>
       </div>
 
       <div className={styles.understanding}>
         <div>
-          <div className="heading-1">Strengths</div>
           <div className={styles.journal}>
+            <div className="heading-1">Strengths</div>
             <RichText placeholder="What are your strengths?" />
           </div>
         </div>
 
         <div>
-          <div className="heading-1">Weaknesses</div>
           <div className={styles.journal}>
+            <div className="heading-1">Weaknesses</div>
             <RichText placeholder="What are your weaknesses?" />
           </div>
         </div>
 
         <div>
-          <div className="heading-1">Likes</div>
           <div className={styles.journal}>
+            <div className="heading-1">Likes</div>
             <RichText placeholder="What makes you happy?" />
           </div>
         </div>
 
         <div>
-          <div className="heading-1">Dislikes</div>
           <div className={styles.journal}>
+            <div className="heading-1">Dislikes</div>
             <RichText placeholder="What makes you unhappy?" />
           </div>
         </div>
