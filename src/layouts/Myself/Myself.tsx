d6846b53875d2ff5ecff9154715e6f9439d8df53
Ricky Lung <dard12@gmail.com>
2019-11-06 01:44:29 -0500

    some grid

--- src/layouts/Myself/Myself.tsx
+++ src/layouts/Myself/Myself.tsx
@@ -8,22 +8,25 @@ function Myself(props: MyselfProps) {
   return (
     <div className={styles.myselfPage}>
       <Note type="values" title="Values" placeholder="What are your values?" />
-      <Note
-        type="strengths"
-        title="Strengths"
-        placeholder="What are your strengths?"
-      />
-      <Note
-        type="weaknesses"
-        title="Weaknesses"
-        placeholder="What are your weaknesses?"
-      />
-      <Note type="likes" title="Likes" placeholder="What makes you happy?" />
-      <Note
-        type="dislikes"
-        title="Dislikes"
-        placeholder="What makes you unhappy?"
-      />
+
+      <div className={styles.myselfGrid}>
+        <Note
+          type="strengths"
+          title="Strengths"
+          placeholder="What are your strengths?"
+        />
+        <Note
+          type="weaknesses"
+          title="Weaknesses"
+          placeholder="What are your weaknesses?"
+        />
+        <Note type="likes" title="Likes" placeholder="What makes you happy?" />
+        <Note
+          type="dislikes"
+          title="Dislikes"
+          placeholder="What makes you unhappy?"
+        />
+      </div>
     </div>
   );
 }
