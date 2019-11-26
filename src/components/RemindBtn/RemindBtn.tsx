1bbe932dd00f3f0e771968c2bcd8fdee1849a11f
Ricky Lung <dard12@gmail.com>
2019-11-25 22:13:46 -0500

    add modal button for reminder

--- src/components/RemindBtn/RemindBtn.tsx
+++ src/components/RemindBtn/RemindBtn.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { IoIosRefresh, IoIosArrowDown } from 'react-icons/io';
 import styles from './RemindBtn.module.scss';
 import { Button } from '../../components/Button/Button';
+import Modal from '../Modal/Modal';
 
 interface RemindBtnProps {}
 
 function RemindBtn(props: RemindBtnProps) {
   return (
     <div className={styles.remindContainer}>
-      <Button className={styles.shuffleBtn}>
-        <IoIosRefresh />
-        Remind Me
-      </Button>
+      <Modal
+        buttonChildren={
+          <Button className={styles.shuffleBtn}>
+            <IoIosRefresh />
+            Remind Me
+          </Button>
+        }
+        render={() => null}
+      />
 
       <Button className={styles.settingBtn}>
         <IoIosArrowDown />
