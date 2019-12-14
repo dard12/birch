d5ad0eda49b691db2986437e11e8b54136fd2dfb
Ricky Lung <dard12@gmail.com>
2019-12-14 11:52:31 +0800

    start on event creation

--- src/containers/EventCreate/EventCreate.tsx
+++ src/containers/EventCreate/EventCreate.tsx
@@ -1,27 +1,115 @@
-import React from 'react';
-import { IoIosCalendar } from 'react-icons/io';
+import React, { useState } from 'react';
+import { IoIosCalendar, IoIosAdd } from 'react-icons/io';
+import _ from 'lodash';
+import { connect } from 'react-redux';
 import styles from './EventCreate.module.scss';
 import { Button } from '../../components/Button/Button';
 import Modal from '../../components/Modal/Modal';
+import { Input } from '../../components/Input/Input';
+import { Select } from '../../components/Select/Select';
+import { axiosPost } from '../../hooks/useAxios';
+import { loadDocsAction } from '../../redux/actions';
 
-interface EventButtonProps {}
+interface EventButtonProps {
+  loadDocsAction?: Function;
+}
+
+const activities = [
+  'Board Games',
+  'Drinks',
+  'Hangout',
+  'Concert',
+  'Gym',
+  'Food',
+  'Coffee',
+  'Movie',
+];
+const activityOptions = _.map(activities, value => ({ value, label: value }));
 
 function EventButton(props: EventButtonProps) {
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+  const [activity, setActivity] = useState();
+  const [friends, setFriends] = useState();
+
+  const titleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const title = event.currentTarget.value;
+    setTitle(title);
+  };
+
+  const createOnClick = () => {
+    axiosPost(
+      '/api/event',
+      { title, date, time, activity, friends },
+      { collection: 'event', loadDocsAction },
+    );
+  };
+
   return (
     <div className={styles.eventContainer}>
       <Modal
         buttonRender={openModal => (
           <Button className={styles.eventBtn} onClick={openModal}>
             <IoIosCalendar />
-            Create Event
+            New Event
           </Button>
         )}
         modalRender={closeModal => (
-          <div className={styles.eventModal}>hello</div>
+          <div className={styles.eventModal}>
+            <div className={styles.modalContent}>
+              <Input
+                placeholder="Event Title"
+                className={styles.eventTitle}
+                value={title}
+                onChange={titleOnChange}
+              />
+
+              <div className={styles.datetimeRow}>
+                <Select
+                  placeholder="Date"
+                  options={[{ label: 'Person', value: 'person' }]}
+                  value={date}
+                  onChange={setDate}
+                />
+
+                <Select
+                  placeholder="Time"
+                  options={[{ label: 'Person', value: 'person' }]}
+                  value={time}
+                  onChange={setTime}
+                />
+              </div>
+
+              <Select
+                placeholder="Activity"
+                options={activityOptions}
+                value={activity}
+                onChange={setActivity}
+              />
+
+              <Select
+                placeholder="Invite Friends"
+                options={[{ label: 'Person', value: 'person' }]}
+                value={friends}
+                onChange={setFriends}
+              />
+
+              <div className={styles.createRow}>
+                <Button className={styles.createButton} onClick={createOnClick}>
+                  <IoIosAdd />
+                  Create Event
+                </Button>
+              </div>
+            </div>
+          </div>
         )}
       />
     </div>
   );
 }
 
-export default EventButton;
+export default connect(
+  null,
+  { loadDocsAction },
+)(EventButton);
