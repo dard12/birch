9c35868b054115bae6d5eb7263697346760d463e
Ricky Lung <dard12@gmail.com>
2019-12-14 18:37:08 +0800

    add back date picker

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import Flatpickr from 'react-flatpickr';
-// import styles from './DatePicker.module.scss';
-// import 'flatpickr/dist/themes/light.css';
+import React from 'react';
+import Flatpickr from 'react-flatpickr';
+import styles from './DatePicker.module.scss';
+import 'flatpickr/dist/themes/light.css';
 
-// interface DatePickerProps {}
+interface DatePickerProps {}
 
-// export function DatePicker(props: DatePickerProps & any) {
-//   const { className = styles.datetime, ...passedProps } = props;
+export function DatePicker(props: DatePickerProps & any) {
+  const { className = styles.datetime, ...passedProps } = props;
 
-//   return (
-//     <Flatpickr
-//       className={className}
-//       options={{ dateFormat: 'M j, Y' }}
-//       {...passedProps}
-//     />
-//   );
-// }
+  return (
+    <Flatpickr
+      className={className}
+      options={{ dateFormat: 'M j, Y' }}
+      {...passedProps}
+    />
+  );
+}
--- src/containers/EventCreate/EventCreate.tsx
+++ src/containers/EventCreate/EventCreate.tsx
@@ -1,25 +1,104 @@
-import React from 'react';
-import { IoIosCalendar } from 'react-icons/io';
+import React, { useState } from 'react';
+import { IoIosCalendar, IoIosAdd } from 'react-icons/io';
+import _ from 'lodash';
+import { connect } from 'react-redux';
 import styles from './EventCreate.module.scss';
 import { Button } from '../../components/Button/Button';
+import Modal from '../../components/Modal/Modal';
+import { Select } from '../../components/Select/Select';
+import { axiosPost } from '../../hooks/useAxios';
+import { loadDocsAction } from '../../redux/actions';
+import { DatePicker } from '../../components/DatePicker/DatePicker';
 
 interface EventButtonProps {
   loadDocsAction?: Function;
 }
 
+const activities = [
+  'Board Games',
+  'Drinks',
+  'Hangout',
+  'Gym',
+  'Food',
+  'Coffee',
+  'Movie',
+];
+const activityOptions = _.map(activities, value => ({ value, label: value }));
+
 function EventButton(props: EventButtonProps) {
+  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState();
+  const [activity, setActivity] = useState();
+  const [friends, setFriends] = useState();
+
+  const createOnClick = () => {
+    axiosPost(
+      '/api/event',
+      { date, time, activity, friends },
+      { collection: 'event', loadDocsAction },
+    );
+  };
+
   return (
-    <a
-      href="https://calendar.google.com/calendar/r"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <Button className={styles.eventBtn}>
-        <IoIosCalendar />
-        See Calendar
-      </Button>
-    </a>
+    <div className={styles.eventContainer}>
+      <Modal
+        buttonRender={openModal => (
+          <Button className={styles.eventBtn} onClick={openModal}>
+            <IoIosCalendar />
+            New Event
+          </Button>
+        )}
+        modalRender={closeModal => (
+          <div className={styles.eventModal}>
+            <div className={styles.modalContent}>
+              <div className={styles.datetimeRow}>
+                <DatePicker
+                  placeholder="Date"
+                  value={date}
+                  onChange={setDate}
+                />
+
+                <DatePicker
+                  placeholder="Time"
+                  value={time}
+                  onChange={setTime}
+                  options={{
+                    enableTime: true,
+                    noCalendar: true,
+                    dateFormat: 'H:i',
+                  }}
+                />
+              </div>
+
+              <Select
+                placeholder="Activity"
+                options={activityOptions}
+                value={activity}
+                onChange={setActivity}
+              />
+
+              <Select
+                placeholder="Invite Friends"
+                options={[{ label: 'Person', value: 'person' }]}
+                value={friends}
+                onChange={setFriends}
+              />
+
+              <div className={styles.createRow}>
+                <Button className={styles.createButton} onClick={createOnClick}>
+                  <IoIosAdd />
+                  Create Event
+                </Button>
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    </div>
   );
 }
 
-export default EventButton;
+export default connect(
+  null,
+  { loadDocsAction },
+)(EventButton);
